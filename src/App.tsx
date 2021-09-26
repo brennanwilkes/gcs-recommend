6705717cd7f140feb5b7bc037f337f0a430ede3e
Brennan Wilkes <brennan@codexwilkes.com>
2021-09-26 16:44:11 -0700

    fix: Removed persistent scroll

--- src/App.tsx
+++ src/App.tsx
@@ -113,6 +113,10 @@ export default class App extends React.Component<IProps, IState>{
 				auth: true
 			});
 		}
+		window.scrollTo(0, 0);
+		window.onbeforeunload = function () {
+			window.scrollTo(0, 0);
+		}
 	}
 
 	render(){
